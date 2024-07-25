6233a78754a1b7ce9d5a3ca230f2132ef7ceec2f
cesare cazzaniga <ccazzaniga@student.ethz.ch>
2024-07-25 23:46:51 +0200

    removing function that cannot work, replacing by TBD

--- analysis_configs/sequences_s_channel_leptons.py
+++ analysis_configs/sequences_s_channel_leptons.py
@@ -10,61 +10,8 @@ from analysis_configs import objects_definition_s_channel_leptons as obj
 from utils.Logger import *
 
 
-def remove_primary_dataset_overlap(events, year, primary_dataset):
-
-    def trigger_list_to_index_list(year, primary_dataset):
-        return [trigger_table[trigger] for trigger in primary_dataset_triggers[primary_dataset][year]]
-
-    def is_passing_primary_dataset_triggers(events, year, primary_dataset):
-        trigger_indices = trigger_list_to_index_list(year, primary_dataset)
-        primary_dataset_trigger_mask = np.zeros(len(events.TriggerPass[0]), dtype=int)
-        primary_dataset_trigger_mask[trigger_indices] = 1
-        primary_dataset_trigger_mask = np.tile(primary_dataset_trigger_mask, [len(events), 1])
-        return ak.any(events.TriggerPass * primary_dataset_trigger_mask, axis=1)
-
-    pass_jetht_triggers = is_passing_primary_dataset_triggers(events, year, "JetHT")
-    pass_met_triggers = is_passing_primary_dataset_triggers(events, year, "MET")
-
-    if primary_dataset == "JetHT":
-        overlap_mask = np.ones(len(events), dtype=bool)
-    elif primary_dataset == "MET":
-        overlap_mask = np.bitwise_not(pass_jetht_triggers)
-    elif primary_dataset == "HTMHT":
-        overlap_mask = np.bitwise_not(pass_jetht_triggers | pass_met_triggers)
-    else:
-        log.critical(f"Invalid primary dataset name {primary_dataset}!")
-        exit(1)
-    
-    return events[overlap_mask]
-
-
-def remove_single_lepton_primary_dataset_overlap(events, year, primary_dataset):
-
-    def trigger_list_to_index_list(year, primary_dataset):
-        return [trigger_table[trigger] for trigger in primary_dataset_triggers[primary_dataset][year]]
-
-    def is_passing_primary_dataset_triggers(events, year, primary_dataset):
-        trigger_indices = trigger_list_to_index_list(year, primary_dataset)
-        primary_dataset_trigger_mask = np.zeros(len(events.TriggerPass[0]), dtype=int)
-        primary_dataset_trigger_mask[trigger_indices] = 1
-        primary_dataset_trigger_mask = np.tile(primary_dataset_trigger_mask, [len(events), 1])
-        return ak.any(events.TriggerPass * primary_dataset_trigger_mask, axis=1)
-
-    pass_single_muon_triggers = is_passing_primary_dataset_triggers(events, year, "SingleMuon")
-    if year in ["2016", "2016APV", "2017"]:
-        single_electron_trigger = "SingleElectron"
-    else:
-        single_electron_trigger = "EGamma"
-
-    if primary_dataset == "SingleMuon":
-        overlap_mask = np.ones(len(events), dtype=bool)
-    elif primary_dataset == single_electron_trigger:
-        overlap_mask = np.bitwise_not(pass_single_muon_triggers)
-    else:
-        log.critical(f"Invalid primary dataset name {primary_dataset}!")
-        exit(1)
-    
-    return events[overlap_mask]
+#CZZ: to  be implemented: remove_primary_dataset_overlap
+#CZZ: to  be implemented: remove_single_lepton_primary_dataset_overlap
 
 
 def apply_good_ak8_jet_filter(events):
@@ -110,39 +57,9 @@ def add_good_ak8_jet_branch(events):
     return events
 
 
-def apply_lepton_veto(
-        events,
-        electron_extra_condition=None,
-        muon_extra_condition=None,
-    ):
+#CZZ: to  be implemented: apply_lepton_veto
 
-    electron_condition = obj.is_veto_electron(events.Electrons)
-    if electron_extra_condition is not None:
-        electron_condition = electron_condition & electron_extra_condition
-
-    muon_condition = obj.is_veto_muon(events.Muons)
-    if muon_extra_condition is not None:
-        muon_condition = muon_condition & muon_extra_condition
-
-    veto_electrons = events.Electrons[electron_condition]
-    veto_muons = events.Muons[muon_condition]
-    n_veto_electrons = ak.count(veto_electrons.pt, axis=1)
-    n_veto_muons = ak.count(veto_muons.pt, axis=1)
-    n_veto_leptons = n_veto_electrons + n_veto_muons
-    events = events[n_veto_leptons == 0]
-    return events
-
-
-def require_n_veto_leptons(events, n):
-    is_veto_electron = obj.is_veto_electron(events.Electrons)
-    is_veto_muon = obj.is_veto_muon(events.Muons)
-    veto_electrons = events.Electrons[is_veto_electron]
-    veto_muons = events.Muons[is_veto_muon]
-    n_veto_electrons = ak.count(veto_electrons.pt, axis=1)
-    n_veto_muons = ak.count(veto_muons.pt, axis=1)
-    n_veto_leptons = n_veto_electrons + n_veto_muons
-    events = events[n_veto_leptons == n]
-    return events
+#CZZ: to  be implemented: require_n_veto_leptons
 
 
 def add_analysis_branches(events):
