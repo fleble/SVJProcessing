15d17bbf42aced2f2832f70a8e041036133d8d43
Florian Eble <florian.eble@cern.ch>
2024-08-12 16:47:52 +0200

    Syncing with new code from PR 30

--- prepare_input_files_lists/list_dataset_files.py
+++ prepare_input_files_lists/list_dataset_files.py
@@ -2,6 +2,7 @@ import argparse
 import csv 
 from importlib import import_module
 from pathlib import Path
+from functools import partial
 
 import uproot
 
@@ -49,54 +50,44 @@ def __get_arguments():
     return parser.parse_args()
 
 
-def __run_bash_command(bash_command):
-    return subprocess.Popen(bash_command, shell=True, stdout=subprocess.PIPE).stdout.read().decode("utf-8")[:-1]
-
-
-def __get_files_list_from_info_dict(info_dict,nano_aod):
-    redirector = info_dict["redirector"]
-    path = info_dict["path"]
-    regex = info_dict["regex"]
-    bash_command = f"xrdfs {redirector} ls {path} | grep -E \"{regex}\" | sort -n"
-    files_list = __run_bash_command(bash_command).split("\n")
-    files_list = [f"{redirector}{file_name}" for file_name in files_list]
-    files_list = [file_name for file_name in files_list if "log" not in file_name]
-    files_list = [file_name for file_name in files_list if "test.sh" not in file_name]
-    if nano_aod:
-        if "nano_data" in files_list[0].split("/")[-1]:
-            files_list = sorted(files_list, key=lambda x: int(x.split("/")[-1].split("_")[2].replace(".root","")))
-        elif "PFNanoSuper" in files_list[0].split("/")[-1]:
-            files_list = sorted(files_list, key=lambda x: int(x.split("/")[-1].split("-")[2].replace(".root","")))
-        elif "PFNanoAOD_SVJL_" in files_list[0].split("/")[-1]:
-            files_list = sorted(files_list, key=lambda x: int(x.split("/")[-1].split("-")[6].replace(".root","")))
-        elif "PFNanoAOD_SVJtaus_" in files_list[0].split("/")[-1]:
-            files_list = sorted(files_list, key=lambda x: int(x.split("/")[-1].split("-")[6].replace(".root","")))
-        elif "PFNANOAOD" in files_list[0].split("/")[-1]:
-            files_list = sorted(files_list, key=lambda x: int(x.split("/")[-1].split("-")[2].replace(".root","")))
-        elif "PFNanoAOD" in files_list[0].split("/")[-1]:
-            files_list = sorted(files_list, key=lambda x: int(x.split("/")[-1].split("-")[2].replace(".root","")))
-        else:
-            files_list = sorted(files_list, key=lambda x: int(x.split("/")[-1].split("_")[0]))
-    else:
-        files_list = sorted(files_list, key=lambda x: int(x.split("/")[-1].split("_")[0]))
-    return files_list
-
-
-def __list_files(dataset_info,nano_aod):
+def __list_files(dataset_info, nano_aod):
     files_list = []
     for info_dict in dataset_info:
-        files_list += __get_files_list_from_info_dict(info_dict, nano_aod)
+        files_list_ = get_files_list(
+            path=info_dict["path"],
+            redirector=info_dict["redirector"],
+            regex=info_dict["regex"],
+        )
 
+        # TODO: Do something more clever should be done insteead of relying of the file name
+        try:
+            if nano_aod:
+                if "nano_data" in files_list[0].split("/")[-1]:
+                    files_list_ = sorted(files_list_, key=lambda x: int(x.split("/")[-1].split("_")[2].replace(".root","")))
+                elif "PFNanoSuper" in files_list[0].split("/")[-1]:
+                    files_list_ = sorted(files_list_, key=lambda x: int(x.split("/")[-1].split("-")[2].replace(".root","")))
+                elif "PFNanoAOD_SVJL_" in files_list[0].split("/")[-1]:
+                    files_list_ = sorted(files_list_, key=lambda x: int(x.split("/")[-1].split("-")[6].replace(".root","")))
+                elif "PFNanoAOD_SVJtaus_" in files_list[0].split("/")[-1]:
+                    files_list_ = sorted(files_list_, key=lambda x: int(x.split("/")[-1].split("-")[6].replace(".root","")))
+                elif "PFNANOAOD" in files_list[0].split("/")[-1]:
+                    files_list_ = sorted(files_list_, key=lambda x: int(x.split("/")[-1].split("-")[2].replace(".root","")))
+                elif "PFNanoAOD" in files_list[0].split("/")[-1]:
+                    files_list_ = sorted(files_list_, key=lambda x: int(x.split("/")[-1].split("-")[2].replace(".root","")))
+                else:
+                    files_list_ = sorted(files_list_, key=lambda x: int(x.split("/")[-1].split("_")[0]))
+            else:
+                files_list_ = sorted(files_list_, key=lambda x: int(x.split("/")[-1].split("_")[0]))
+        except:
+            pass
+
+        files_list += files_list_
 
     return files_list
 
 
-def __get_number_of_events(file_name, nano_aod, tree_name=""):  
+def __get_number_of_events(file_name, tree_name):
     file_ = uproot.open(file_name)
-    if nano_aod:
-        tree_name = "Events"
-    else:
-        tree_name = "TreeMaker2/PreSelection"
     events = file_[tree_name]
     f0 = events.keys()[0]
     return events[f0].num_entries
@@ -111,23 +102,22 @@ def __write_dataset_info(
         nano_aod=False,
     ):
     
-    files_list = __list_files(dataset_info,nano_aod)
+    files_list = __list_files(dataset_info, nano_aod)
 
     output_directory_ = f"{output_directory}/files_list/{year}"
     Path(output_directory_).mkdir(parents=True, exist_ok=True)
 
+    if nano_aod:
+        tree_name = "Events"
+    else:
+        tree_name = "TreeMaker2/PreSelection"
 
-    files_list_batches = []
-    max_n_files = 5000
-    for i in range(1 + len(files_list)//max_n_files):
-        files_list_batches.append(files_list[i * max_n_files: (i+1) * max_n_files])
-
-    number_of_events = []
-    for i, files_list_batch in enumerate(files_list_batches):
-        log.info(f"Processing batch {i+1}/{len(files_list_batches)}...")
-        with concurrent.futures.ProcessPoolExecutor(max_workers=n_workers) as executor:
-            number_of_events += list(tqdm(executor.map(__get_number_of_events, files_list_batch, len(files_list_batch)*[nano_aod]), total=len(files_list_batch)))
-
+    process_function = partial(__get_number_of_events, tree_name=tree_name)
+    number_of_events = process_in_parallel(
+        files_list=files_list,
+        process_function=process_function,
+        n_workers=n_workers,
+    )
 
     output_file_name = f"{output_directory_}/{dataset}.csv"
     header = ["file_name", "number_of_events"]
